3ad085f8272d3bf154d37c4817db4e9be3086124
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-05-24 08:42:48 -0400

    Updated from Brython Server: 5/24/2018 8:42:47 AM

--- goL.py
+++ goL.py
@@ -4,5 +4,25 @@
 
 from ggame import *
 
+def buildBoard():
+ [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
+
+
+def redrawAll():
+     for r in range(0,10):
+        for c in range (0,10):
+            Sprite(buildBoard[r][c])
+        
+    
 
 if __name__ == '__main__': # setup and runs game, just put all the def functions before
+
+#colors for the color god
+    green = Color(0x00FF00,1) #Green
+    white = Color(0xFFFFFF,1) #white
+    black = Color(0x000000,1) #Black
+    
+    deadSquare = RectangleAsset(30,30, LineStyle(1,white),black)
+    buildBoard()
+    redrawAll()
+    App().run()
